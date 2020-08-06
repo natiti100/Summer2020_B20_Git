77e3622a3f52a5a3fd1e43e9cff0bb3453f02e65
natiti100 <68876450+natiti100@users.noreply.github.com>
2020-08-05 21:32:18 -0400

    Update GitFun.java

--- src/Utility/GitFun.java
+++ src/Utility/GitFun.java
@@ -3,7 +3,7 @@ package Utility;
 public class GitFun {
 
     public static void main(String[] args) {
-        System.out.println("Everything is fun");
+        System.out.println("Everything is fun");// more practice
     }
 
 }
